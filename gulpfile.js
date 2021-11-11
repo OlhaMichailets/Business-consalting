abfbb08524dc2193f3c632e43a2a2b1d15ca46ee
Olha <32494251+OlhaMichailets@users.noreply.github.com>
2021-11-11 12:08:23 +0200

    added csso into gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,32 @@
 
 const { src, dest } = require('gulp');
+const plumber = require('gulp-plumber')
 const sourcemaps = require('gulp-sourcemaps')
 const sass = require('gulp-sass')(require('sass'))
 const autoprefixer = require('gulp-autoprefixer')
+const csso = require('gulp-csso')
 const rename = require('gulp-rename')
-const plumber = require('gulp-plumber')
+
 
 
 function html () {
     return src('source/*.html')
         .pipe(dest('build'))
 }
-exports.html = html
 
 function css () {
-        return src('source/scss/**/*.scss')
-            .pipe(plumber())
-            .pipe(sourcemaps.init())
-            .pipe(sass())
-            .pipe(autoprefixer())
-            .pipe(rename('style.min.css'))
-            .pipe(sourcemaps.write('./'))
-            .pipe(dest('build/css'))
+    return src('source/scss/**/*.scss')
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(autoprefixer())
+        .pipe(csso())
+        .pipe(rename('style.min.css'))
+        .pipe(sourcemaps.write('./'))
+        .pipe(dest('build/css'))
 }
+
+
+
+exports.html = html
 exports.css = css
