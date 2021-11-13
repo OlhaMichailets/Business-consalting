07146f75b3ec4ea3aa8dfb580e5910edefcb0a2a
Olha <32494251+OlhaMichailets@users.noreply.github.com>
2021-11-13 12:25:17 +0200

    bug: don't create img folder in build direction

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 
-const { src, dest } = require('gulp');
+const { src, dest, watch, series } = require('gulp');
 const plumber = require('gulp-plumber')
 const sourcemaps = require('gulp-sourcemaps')
 const sass = require('gulp-sass')(require('sass'))
 const autoprefixer = require('gulp-autoprefixer')
 const csso = require('gulp-csso')
 const rename = require('gulp-rename')
+const server = require('browser-sync')
+const imagemin = require('gulp-imagemin')
 
 
 
@@ -26,7 +28,44 @@ function css () {
         .pipe(dest('build/css'))
 }
 
+function cssNomin () {
+    return src('source/scss/style.scss')
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(autoprefixer())
+        .pipe(dest('build/css'))
+}
+
+function serve () {
+    server.init({
+        server: "build/",
+        notify: false,
+        open: true,
+        cors: true,
+        ui: false
+    })
+    watch('source/scss/**/*.scss', series(css, cssNomin, refresh))
+    watch('source/*.html', series(html, refresh))
+}
+
+function refresh (done) {
+    server.reload()
+    done()
+}
+
+function images () {
+    return src('source/img/**/*.{png,jpg,jpeg}')
+        pipe(imagemin([
+            imagemin.optipng({optimizationLevel1: 3}),
+            imagemin.mozjpeg({progressive: true})
+        ])).pipe(dest('build/img'))
+}
+
 
 
 exports.html = html
 exports.css = css
+exports['css-nomin'] = cssNomin
+exports.serve = serve
+exports.images = images
+
