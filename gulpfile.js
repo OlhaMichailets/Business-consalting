eff6744788995976840b99c99722857c6f778eb0
Olha <32494251+OlhaMichailets@users.noreply.github.com>
2021-11-18 13:42:35 +0200

    created images for mobile and desktop version taking into account retina-displays

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 
-const { src, dest, watch, series } = require('gulp');
+const { src, dest, watch, series, parallel } = require('gulp');
+const htmlmin = require ('gulp-htmlmin')
 const plumber = require('gulp-plumber')
 const sourcemaps = require('gulp-sourcemaps')
 const sass = require('gulp-sass')(require('sass'))
@@ -7,26 +8,31 @@ const autoprefixer = require('gulp-autoprefixer')
 const csso = require('gulp-csso')
 const rename = require('gulp-rename')
 const server = require('browser-sync')
-const imagemin = require('gulp-imagemin')
+const imageMin = require('gulp-imagemin')
+const webp = require('gulp-webp')
 const svgstore = require('gulp-svgstore')
 
 const pipeline = require('readable-stream').pipeline
 const uglify = require('gulp-uglify-es').default
-const del = require('del')
+const del = require('del');
+const { default: imagemin } = require('imagemin');
 
 
 
 function html () {
     return src('source/*.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
         .pipe(dest('build'))
 }
 
 function css () {
-    return src('source/scss/**/*.scss')
+    return src('source/scss/style.scss')
         .pipe(plumber())
         .pipe(sourcemaps.init())
         .pipe(sass())
-        .pipe(autoprefixer())
+        .pipe(autoprefixer({
+            cascade: false
+        }))
         .pipe(csso())
         .pipe(rename('style.min.css'))
         .pipe(sourcemaps.write('./'))
@@ -37,7 +43,9 @@ function cssNomin () {
     return src('source/scss/style.scss')
         .pipe(plumber())
         .pipe(sass())
-        .pipe(autoprefixer())
+        .pipe(autoprefixer({
+            cascade: false
+        }))
         .pipe(dest('build/css'))
 }
 
@@ -48,32 +56,56 @@ function serve () {
         open: true,
         cors: true,
         ui: false
-    })
-    watch('source/scss/**/*.scss', series(css, cssNomin, refresh))
-    watch('source/*.html', series(html, refresh))
+    });
+
+    watch('source/scss/**/*.scss', series(css, cssNomin, refresh));
+    watch('source/*.html', series(html, refresh));
+    watch('source/js/*.js', series(js, refresh));
+    watch('source/img/icon-*.svg', series(sprite, refresh));
+    watch('source/img/**/*.{png,jpeg,jpg}', series(images, getWebp, refresh));
 }
 
 function refresh (done) {
-    server.reload()
-    done()
+    server.reload();
+    done();
 }
 
 function images () {
-    return src('source/img/**/*.{png,jpg,jpeg}')
-        .pipe(imagemin([
-            imagemin.optipng({optimizationLevel1: 3}),
-            imagemin.mozjpeg({progressive: true})
+    return src('source/img/**/*.{png,jpeg,jpg}')
+        .pipe(imageMin([
+            imageMin.optipng({optimizationLevel1: 3}),
+            imageMin.mozjpeg({progressive: true})
         ])).pipe(dest('build/img'))
+}
+
+function getWebp () {
+    return src('build/img/**/*.{png,jpeg,jpg}')
+        .pipe(webp({ quality: 90}))
+        .pipe(dest('build/img'))
 }
 
 function sprite () {
     return src('source/img/icon-*.svg')
-        .pipe(imagemin([imagemin.svgo()]))
+        .pipe(imageMin([imageMin.svgo()]))
         .pipe(svgstore({
             inlineSvg: true
         }))
         .pipe(rename('sprite.svg'))
         .pipe(dest('build/img'))
+}
+
+function copy () {
+    return src([
+        'source/fonts/**/*',
+        'source/js/*',
+        '!source/img/*',
+        '!source/img/icon-*.svg',
+        'source/bg/*',
+        'source/*.ico*'
+    ], {
+        base: 'source'
+    })
+        .pipe(dest('build'))
 }
 
 function js () {
@@ -87,13 +119,47 @@ function js () {
     )
 }
 
+function clean () {
+    return del('build')
+}
+
+exports.build = series(
+    clean,
+    images,
+    getWebp,
+    parallel(
+        copy,
+        html,
+        css,
+        cssNomin,
+        sprite,
+        js
+    )
+);
+exports.start = series(
+    series(
+        clean,
+        images,
+        getWebp,
+        parallel(
+            copy,
+            html,
+            css,
+            cssNomin,
+            sprite,
+            js
+        )
+    ),
+    serve
+);
 
 
-exports.html = html
-exports.css = css
-exports['css-nomin'] = cssNomin
-exports.serve = serve
-exports.images = images
-exports.sprite = sprite
-exports.js = js
-
+exports.serve = serve;
+exports.images = images;
+exports.getWebp = getWebp;
+exports.sprite = sprite;
+exports.js = js;
+exports.html = html;
+exports.css = css;
+exports['css-nomin'] = cssNomin;
+exports.refresh = refresh;
