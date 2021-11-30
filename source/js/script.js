1691f4f92e41040db477c981fa3280eccc4742d3
Olha <32494251+OlhaMichailets@users.noreply.github.com>
2021-11-30 10:17:28 +0200

    scalable images

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,13 @@
-// swiper
-new Swiper('.mySwiper', {
-    autoHeight: true,
-    slidesPerView: 2,
-});
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('sdasdsad');
+    var swiper = new Swiper(
+        ".mySwiper",
+        {
+          slidesPerView: 1.3,
+          spaceBetween: 10,
+        },
+      );
+}
+);
 
