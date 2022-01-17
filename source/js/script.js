025c6ce99494d65938b4992b7a1b17167185a911
Olha <32494251+OlhaMichailets@users.noreply.github.com>
2022-01-17 20:14:37 +0200

    swiper wip

--- source/js/script.js
+++ source/js/script.js
@@ -1,12 +1,42 @@
-
 document.addEventListener('DOMContentLoaded', function() {
-    var swiper = new Swiper(
-        ".mySwiper",
-        {
-          slidesPerView: 1.3,
-          spaceBetween: 10,
-        },
-      );
-}
-);
+  new Swiper('.mySwiper', {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    },
+  //   pagination: {
+  //     el: '.swiper-pagination',
+  //     type: 'fraction'
+  //   },
+
+  },
+  );
+
+  // new Swiper('.mySwiper', {
+  //   // navigation: {
+  //   //   nextEl: '.swiper-button-next',
+  //   //   prevEl: '.swiper-button-prev'
+  //   // },
+  //   slidesPerView: 10,
+  //   spaceBetween: 10,
+  //   // pagination: {
+  //   //   el: '.swiper-pagination',
+  //   //   type: 'fraction'
+  //   // },
+  // },
+  // );
+
+});
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     var swiper = new Swiper(
+//         ".mySwiper",
+//         {
+//           slidesPerView: 1.3,
+//           spaceBetween: 10,
+//         },
+//       );
+// });
 
